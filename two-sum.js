93f2b06e4b22112027bab499c598bf18f2b973fa
Justin-Arakaki <Justiniankoi@gmail.com>
2023-10-10 18:36:26 -0700

    improved two-sum

--- two-sum.js
+++ two-sum.js
@@ -3,21 +3,18 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-	for (let index = 0; index < nums.length; index++) {
-		const current = nums[index];
-		if (current > target - 2) continue;
 
-		for (let j = index + 1; j < nums.length; j++) {
-			const sum = current + nums[j];
-			console.log(current, nums[j], sum);
-			if (sum === target) return [index, j];
+const twoSum = function (nums, target) {
+	const comp = new Map();
+	for (let i = 0; i < nums.length; i++) {
+		const currentComp = nums[i];
+
+		if (comp.has(currentComp)) {
+			return [comp.get(currentComp), i];
 		}
-	}
 
-	return false;
+		comp.set(target - nums[i], i);
+	}
 };
 
-console.log('ANSWER', twoSum([2, 7, 11, 15], 9));
-console.log('ANSWER', twoSum([3, 2, 4], 6));
-console.log('ANSWER', twoSum([3, 3], 6));
+console.log('ANSWER', twoSum([2, 7, 11, 15], 22));
